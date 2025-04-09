886afca79044efd2333ecd8a8845afdf72671b7e
kylehipz <kylehipolito2109@gmail.com>
2025-04-09 15:41:44 +0800

    feat: Docker compose + AIR

--- auth/main.go
+++ auth/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/kylehipz/blogapp-microservices/libs/pkg/api"
 
 	"github.com/kylehipz/blogapp-microservices/auth/internal"
 )
 
 func main() {
-	apiServer := api.NewEchoAPIServer(internal.API_SERVER_PORT)
+	apiServerPort := fmt.Sprintf(":%s", internal.API_SERVER_PORT)
+	apiServer := api.NewEchoAPIServer(apiServerPort)
 
 	routes := []*api.EchoAPIRoute{}
 	apiServer.Run(routes)
