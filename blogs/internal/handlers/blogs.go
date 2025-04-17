07093dcdbc87d56cc54cd285d5ec958d6e301d28
kylehipz <kylehipolito2109@gmail.com>
2025-04-17 21:16:10 +0800

    feat: Create blog

--- blogs/internal/handlers/blogs.go
+++ blogs/internal/handlers/blogs.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/kylehipz/blogapp-microservices/libs/pkg/middlewares"
 	"github.com/labstack/echo/v4"
 
 	"github.com/kylehipz/blogapp-microservices/blogs/internal/services"
@@ -12,8 +13,24 @@ type BlogsHandler struct {
 	BlogsService *services.BlogsService
 }
 
+type CreateBlogRequestBody struct {
+	Content string `json:"content"`
+}
+
 func (b *BlogsHandler) CreateBlog(c echo.Context) error {
-	return c.JSON(http.StatusCreated, echo.Map{"notImplemented": true})
+	body := new(CreateBlogRequestBody)
+	author := middlewares.GetUserID(c)
+
+	if err := c.Bind(body); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	createdBlog, err := b.BlogsService.CreateBlog(c.Request().Context(), author, body.Content)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusCreated, createdBlog)
 }
 
 func (b *BlogsHandler) GetBlog(c echo.Context) error {
--- blogs/internal/services/blogs.go
+++ blogs/internal/services/blogs.go
@@ -1,9 +1,33 @@
 package services
 
 import (
+	"context"
+
+	"github.com/google/uuid"
 	"github.com/kylehipz/blogapp-microservices/libs/pkg/db"
 )
 
 type BlogsService struct {
 	Queries *db.Queries
+}
+
+func (b *BlogsService) CreateBlog(
+	ctx context.Context,
+	author string,
+	content string,
+) (*db.Blog, error) {
+	authorID, err := uuid.Parse(author)
+	if err != nil {
+		return nil, err
+	}
+
+	createdBlog, err := b.Queries.CreateBlog(ctx, db.CreateBlogParams{
+		Author:  authorID,
+		Content: content,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &createdBlog, err
 }
