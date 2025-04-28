fc0ad9cc0f6eb6f87b094d53a1c92b6499a11529
kylehipz <kylehipolito2109@gmail.com>
2025-04-28 19:28:11 +0800

    feat: Add domain types

---
+++ libs/pkg/types/domain.go
@@ -0,0 +1,21 @@
+package types
+
+type Blog struct {
+	ID        string `json:"id"`
+	Author    *User  `json:"author"`
+	Title     string `json:"title"`
+	Content   string `json:"content"`
+	CreatedAt string `json:"createdAt"`
+}
+
+type User struct {
+	ID        string `json:"id"`
+	Username  string `json:"Username"`
+	Email     string `json:"Email"`
+	CreatedAt string `json:"createdAt"`
+}
+
+type Follow struct {
+	Follower string `json:"follower"`
+	Followee string `json:"followee"`
+}
