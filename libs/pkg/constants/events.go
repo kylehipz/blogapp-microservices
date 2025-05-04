e8c16272fdb822e022ef0346d3df8e97bab768a5
kylehipz <kylehipolito2109@gmail.com>
2025-05-04 13:58:50 +0800

    feat: Events constants

---
+++ libs/pkg/constants/events.go
@@ -0,0 +1,9 @@
+package constants
+
+const (
+	BLOG_CREATED    string = "blog.created"
+	BLOG_UPDATED    string = "blog.updated"
+	BLOG_DELETED    string = "blog.deleted"
+	USER_FOLLOWED   string = "follow.follow"
+	USER_UNFOLLOWED string = "follow.unfollow"
+)
