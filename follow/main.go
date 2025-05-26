2b1cd2b18fb61ce81057070e6e4144fbd2a3a766
kylehipz <kylehipolito2109@gmail.com>
2025-05-26 16:11:15 +0800

    feat(follow): Use new logger

--- follow/main.go
+++ follow/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/jackc/pgx/v5"
@@ -11,6 +10,7 @@ import (
 	"github.com/kylehipz/blogapp-microservices/libs/pkg/loadenv"
 	"github.com/kylehipz/blogapp-microservices/libs/pkg/middlewares"
 	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
 
 	"github.com/kylehipz/blogapp-microservices/follow/internal/routes"
 )
@@ -22,16 +22,20 @@ func main() {
 		loadenv.Load()
 	}
 
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+
 	// start database
 	ctx := context.Background()
 
 	// start database
 	conn, err := pgx.Connect(ctx, os.Getenv("DATABASE_URL"))
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal("database connection error", zap.Error(err))
 	}
 	defer conn.Close(ctx)
-	log.Println("Successfully connected to the database")
+
+	logger.Info("database connected")
 
 	// start api server
 	apiServerPort := fmt.Sprintf(":%s", os.Getenv("PORT"))
